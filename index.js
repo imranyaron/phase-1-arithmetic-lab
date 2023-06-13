ab9652babac58b0db65aa633bf8c46fd6312df73
Your Name <your.email@example.com>
2023-06-13 16:36:31 +0300

    complete assighnment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
+console.log(multiply);
+let random = Math.ceil(Math.random()*100) + 1
+const num3 = 34
+const num4 = 6
+const mod =  num3%num4
+const set = { 2, 5, 7,13, 20}
+const max = (...set)
+console.log(max)
